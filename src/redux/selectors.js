25e0ad678bcd48114030802c8f5dc49fdb457c53
iryshanykkolaevna <141450700+iryshanykkolaevna@users.noreply.github.com>
2024-04-14 11:46:50 +0300

    chainge

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,10 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { filterReducer } from './filtersSlice';
+import { createSelector } from "@reduxjs/toolkit";
 
-export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filters: filterReducer,
-      },
-})
+export const selectContacts = state => state.contacts.items;
+export const selectNameFilter = state => state.filters.name;
+export const selectLoading = state => state.contacts.loading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+    [selectContacts, selectNameFilter], 
+    (contacts, filterTerm) => {
+        return contacts.filter(
+            contact => contact.name.toLowerCase().includes(filterTerm.toLowerCase())
+        );
+  });
